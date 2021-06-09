1aa149271e2329a721146d96842ed9da2bfa6385
awenelo <56133692+awenelo@users.noreply.github.com>
2021-06-09 09:06:15 -0400

    Add collision checks

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 class Rectangles():
-    def __init__(self, rect1startpos, rect2startpos, rect1size, rect2size, rect1colour, rect2colour, speed):
+    def __init__(self, rect1startpos, rect2startpos, rect1size, rect2size, rect1colour, rect2colour, speed, windowwidth, windowheight):
         self.rect1 = pygame.Rect(rect1startpos, rect1size)
         self.rect2 = pygame.Rect(rect2startpos, rect2size)
         self.rect1colour = rect1colour
         self.rect2colour = rect2colour
         self.speed = speed
+        self.windowrect = pygame.Rect((0, 0), (windowwidth, windowheight))
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -31,6 +32,19 @@ class Rectangles():
         if keys[K_d]:
             self.rect1 = self.rect1.move(self.speed, 0)
             self.rect2 = self.rect2.move(self.speed, 0)
+        if self.rect1.x < self.windowrect.x:
+            self.rect1.x = self.windowrect.x
+            self.rect2.x = self.windowrect.x + self.rect1.w
+        if self.rect1.y < self.windowrect.y:
+            self.rect1.y = self.windowrect.y
+            self.rect2.y = self.windowrect.y
+        if self.rect2.right > self.windowrect.right:
+            self.rect1.x = self.windowrect.right-self.rect2.w-self.rect1.w
+            self.rect2.x = self.windowrect.right-self.rect2.w
+        if self.rect2.bottom > self.windowrect.bottom:
+            self.rect1.y = self.windowrect.bottom-self.rect1.height
+            self.rect2.y = self.windowrect.bottom-self.rect2.height
+
     
     def draw(self, screen):
         pygame.draw.rect(screen, self.rect1colour, self.rect1)
@@ -38,7 +52,7 @@ class Rectangles():
         
 
 
-rects = Rectangles((200, 150), (400, 150), (200, 300), (200, 300), (0, 255, 0), (0, 0, 255), 20)
+rects = Rectangles((200, 150), (400, 150), (200, 300), (200, 300), (0, 255, 0), (0, 0, 255), 20, WIDTH, HEIGHT)
 
 clock = pygame.time.Clock()
 
