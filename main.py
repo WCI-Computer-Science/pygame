320b1c1ad510ab4d9f83c90387a3f3d929d7ee1e
awenelo <56133692+awenelo@users.noreply.github.com>
2021-06-09 07:33:03 -0400

    Swap circle and rectangle, circle now draws above rectanle

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 screen.fill((255, 255, 255))
 
-pygame.draw.circle(screen, (0, 0, 0), (400, 300), 25)
 pygame.draw.rect(screen, (255, 0, 0), ((200, 150), (200, 300)))
+pygame.draw.circle(screen, (0, 0, 0), (400, 300), 25)
 
 while True:
     for event in pygame.event.get():
