feaf0cb9ce9bf1245d1dda330ee2ed791b5f6202
awenelo <56133692+awenelo@users.noreply.github.com>
2021-06-09 07:25:57 -0400

    Add circle to center of screen

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 screen.fill((255, 255, 255))
 
+pygame.draw.circle(screen, (0, 0, 0), (400, 300), 25)
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
