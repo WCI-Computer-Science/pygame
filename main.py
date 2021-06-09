60f7342039a6cf5c91e3bf66c1233800d9897369
awenelo <56133692+awenelo@users.noreply.github.com>
2021-06-09 07:30:50 -0400

    Draw rectangle over circle

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ screen = pygame.display.set_mode((800, 600))
 screen.fill((255, 255, 255))
 
 pygame.draw.circle(screen, (0, 0, 0), (400, 300), 25)
+pygame.draw.rect(screen, (255, 0, 0), ((200, 150), (200, 300)))
 
 while True:
     for event in pygame.event.get():
