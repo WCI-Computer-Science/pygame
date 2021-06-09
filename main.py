403248f76ecdcf7398d0834a8445caf54f255806
awenelo <56133692+awenelo@users.noreply.github.com>
2021-06-09 07:46:47 -0400

    Add second rectangle

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 screen.fill((255, 255, 255))
 rect = pygame.Rect((200, 150), (200, 300))
+rect2 = pygame.Rect((400, 150), (200, 300))
 pygame.draw.rect(screen, (0, 255, 0), rect)
+pygame.draw.rect(screen, (0, 0, 255), rect2)
 
 while True:
     for event in pygame.event.get():
